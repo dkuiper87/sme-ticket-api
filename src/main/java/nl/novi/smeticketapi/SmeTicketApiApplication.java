2a64d278d21f0a5dae97d4e50c0c7334c4a162f8
dkuiper87 <dkuiper87@gmail.com>
2026-07-10 16:13:00 +0200

    Project setup and database configuration

---
+++ src/main/java/nl/novi/smeticketapi/SmeTicketApiApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.smeticketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeTicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmeTicketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/smeticketapi/SmeTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.smeticketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmeTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
