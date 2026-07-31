bc0538128a11f9f8eeca42470ce7da78924f4aa9
dkuiper87 <dkuiper87@gmail.com>
2026-07-31 17:51:52 +0200

    Added endpoints for createTicket, getAllTickets and getTicketById

---
+++ src/main/java/nl/novi/smeticketapi/controllers/TicketController.java
@@ -0,0 +1,50 @@
+package nl.novi.smeticketapi.controllers;
+
+import jakarta.validation.Valid;
+import nl.novi.smeticketapi.dtos.ticket.TicketRequestDTO;
+import nl.novi.smeticketapi.dtos.ticket.TicketResponseDTO;
+import nl.novi.smeticketapi.services.TicketService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tickets")
+public class TicketController {
+    private final TicketService ticketService;
+
+    //Constructor
+    public TicketController(TicketService ticketService) {this.ticketService = ticketService;}
+
+    //Endpoints
+    //GET /tickets - Returns a list of all tickets
+    @GetMapping
+    public ResponseEntity<List<TicketResponseDTO>> getAllTickets() {
+        List<TicketResponseDTO> tickets = ticketService.getAllTickets();
+        return ResponseEntity.ok(tickets);
+    }
+
+    //GET /tickets/{id} - Returns a ticket by id
+    @GetMapping("/{id}")
+    public ResponseEntity<TicketResponseDTO> getTicketById(@PathVariable Long id) {
+        TicketResponseDTO ticket = ticketService.getTicketById(id);
+        return ResponseEntity.ok(ticket);
+    }
+
+
+    //POST /tickets - Creates a new ticket
+    @PostMapping
+    public ResponseEntity<TicketResponseDTO> createTicket(@RequestBody @Valid TicketRequestDTO requestDTO) {
+        TicketResponseDTO newTicket = ticketService.createTicket(requestDTO);
+
+        URI location = org.springframework.web.servlet.support.ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newTicket.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(newTicket);
+    }
+}
--- src/main/java/nl/novi/smeticketapi/services/TicketService.java
+++ src/main/java/nl/novi/smeticketapi/services/TicketService.java
@@ -1,5 +1,12 @@
 package nl.novi.smeticketapi.services;
 
+import nl.novi.smeticketapi.dtos.ticket.TicketRequestDTO;
+import nl.novi.smeticketapi.dtos.ticket.TicketResponseDTO;
+import nl.novi.smeticketapi.entities.CategoryEntity;
+import nl.novi.smeticketapi.entities.CourseEntity;
+import nl.novi.smeticketapi.entities.TicketEntity;
+import nl.novi.smeticketapi.entities.UserEntity;
+import nl.novi.smeticketapi.exceptions.RecordNotFoundException;
 import nl.novi.smeticketapi.mappers.TicketDTOMapper;
 import nl.novi.smeticketapi.repositories.CategoryRepository;
 import nl.novi.smeticketapi.repositories.CourseRepository;
@@ -7,13 +14,15 @@ import nl.novi.smeticketapi.repositories.TicketRepository;
 import nl.novi.smeticketapi.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TicketService {
-    private TicketRepository ticketRepository;
-    private UserRepository userRepository;
-    private CategoryRepository categoryRepository;
-    private CourseRepository courseRepository;
-    private TicketDTOMapper ticketDTOMapper;
+    private final TicketRepository ticketRepository;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final CourseRepository courseRepository;
+    private final TicketDTOMapper ticketDTOMapper;
 
     //Constructor
     public TicketService(
@@ -32,4 +41,65 @@ public class TicketService {
 
     //Methods
 
+    //Method to retrieve a list of all tickets
+    public List<TicketResponseDTO> getAllTickets() {
+        return ticketDTOMapper.mapToDto(ticketRepository.findAll());
+    }
+
+    //Private method to retrieve ticket entity
+    private TicketEntity getTicketEntity(Long id) {
+        TicketEntity existingTicketEntity = ticketRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Ticket " + id + " not found"));
+        return existingTicketEntity;
+    }
+
+    //Private method to retrieve user entity
+    private UserEntity getUserEntity(String username) {
+        UserEntity userEntity = userRepository.findById(username)
+                .orElseThrow(() -> new RecordNotFoundException("User " + username + " not found"));
+        return userEntity;
+    }
+
+    //Private method to retrieve category entity
+    private CategoryEntity getCategoryEntity(Long id) {
+        CategoryEntity categoryEntity = categoryRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Category " + id + " not found"));
+        return categoryEntity;
+    }
+
+    //Private method to retrieve course entity
+    private CourseEntity getCourseEntity(Long id) {
+        CourseEntity courseEntity = courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Course " + id + " not found"));
+        return courseEntity;
+    }
+
+    //Method to retrieve a specific ticket by id
+    public TicketResponseDTO getTicketById(Long id) {
+        TicketEntity ticketEntity = getTicketEntity(id);
+        return ticketDTOMapper.mapToDto(ticketEntity);
+    }
+
+    //Method to create a new ticket
+    public TicketResponseDTO createTicket(TicketRequestDTO requestDTO) {
+        TicketEntity ticketEntity = ticketDTOMapper.mapToEntity(requestDTO);
+        UserEntity studentEntity = getUserEntity(requestDTO.getStudentUsername());
+        ticketEntity.setStudent(studentEntity);
+        CategoryEntity categoryEntity = getCategoryEntity(requestDTO.getCategoryId());
+        ticketEntity.setCategory(categoryEntity);
+        CourseEntity courseEntity = getCourseEntity(requestDTO.getCourseId());
+        ticketEntity.setCourse(courseEntity);
+        ticketEntity = ticketRepository.save(ticketEntity);
+        return ticketDTOMapper.mapToDto(ticketEntity);
+    }
+
+    //Method for sme to claim a ticket
+
+    //Method for sme to change the status of a ticket
+
+    //Method for sme to add tags to a ticket
+
+    //Method to update a ticket
+
+    //Method to delete a ticket
 }
